13d8aeb49025a2484d21a097a82fef9685c04a42
Nazmul Haque <bpinazmul18@gmail.com>
2023-02-27 08:23:50 +0600

    FEAT: Working on WhatGPT3 Section.

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+
 import './feature.css'
 
-const Feature = () => {
-  return <div>Feature</div>
+const Feature = ({ title, text }) => {
+  return (
+    <div className="gpt3__features-container__feature">
+      <div className="gpt3__features-container__feature-title">
+        <div />
+        <h1>{title}</h1>
+      </div>
+      <div className="gpt3__features-container__feature-text">{text}</div>
+    </div>
+  )
+}
+
+Feature.propTypes = {
+  title: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
 }
 
 export default Feature
