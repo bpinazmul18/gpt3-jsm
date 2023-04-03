9a9cc833d131d060764075235e24df8d6800e8dc
Nazmul Haque <bpinazmul18@gmail.com>
2023-04-03 07:06:12 +0600

    WORK: Working on Footer section.

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -5,14 +5,16 @@ import './footer.css'
 const Footer = () => {
   return (
     <div className="gpt3__footer section__padding">
-      <div className="gpt3__footer-heading">
-        <h1 className="gradient__text">
-          Do you want to step in to the future before others
-        </h1>
-      </div>
+      <div className="gpt3__footer-title">
+        <div className="gpt3__footer-heading">
+          <h1 className="gradient__text">
+            Do you want to step in to the future before others
+          </h1>
+        </div>
 
-      <div className="gpt3__footer-btn">
-        <a href="#!">Request Early Access</a>
+        <div className="gpt3__footer-btn">
+          <a href="#!">Request Early Access</a>
+        </div>
       </div>
 
       <div className="gpt3__footer-links">
